1aec5b467bff02b24d40b121efa6a9c89ef9a77a
Hayk Safaryan <hayk_safaryan@epam.com>
2021-08-12 19:43:35 +0200

    connect apollo with next

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,15 +1,29 @@
 import React from "react";
 import Router from "next/router";
+import { ApolloProvider } from "@apollo/client";
 import PageComponent from "../components/PageComponent";
+import withData from "../lib/withData";
 
 Router.events.on("routeChangeStart", () => NProgress.start());
 Router.events.on("routeChangeComplete", () => NProgress.done());
 Router.events.on("routeChangeError", () => NProgress.done());
 
-const _App = ({ Component, pageProps }) => (
-  <PageComponent>
-    <Component {...pageProps} />
-  </PageComponent>
+const _App = ({ Component, pageProps, apollo }) => (
+  <ApolloProvider client={apollo}>
+    <PageComponent>
+      <Component {...pageProps} />
+    </PageComponent>
+  </ApolloProvider>
 );
 
-export default _App;
+_App.getInitialProps = async ({ Component, ctx }) => {
+  let pageProps = {};
+  // make apollo work with next
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+  // get any query variables at a page level
+  pageProps.query = ctx.query;
+};
+
+export default withData(_App);
