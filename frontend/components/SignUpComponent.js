375acc6d6a7441cd29c0cc73478be37c914000a6
Hayk Safaryan <hayk_safaryan@epam.com>
2021-11-21 21:02:01 +0100

    implement sign up

---
+++ frontend/components/SignUpComponent.js
@@ -0,0 +1,97 @@
+import React, { useCallback } from "react";
+import gql from "graphql-tag";
+import { useMutation } from "@apollo/client";
+import Form from "./styles/Form";
+import useForm from "../lib/useForm";
+import DisplayError from "./ErrorMessage";
+
+const SING_UP_MUTATION = gql`
+  mutation SIGNUP_MUTATION(
+    $email: String!
+    $name: String!
+    $password: String!
+  ) {
+    createUser(data: { email: $email, name: $name, password: $password }) {
+      id
+      email
+      name
+    }
+  }
+`;
+
+const SignUpComponent = () => {
+  const { inputs, handleChange, resetForm } = useForm({
+    email: "",
+    name: "",
+    password: "",
+  });
+
+  const [signup, { data, loading, error }] = useMutation(SING_UP_MUTATION, {
+    variables: inputs,
+  });
+
+  const handleSubmit = useCallback(
+    async (e) => {
+      e.preventDefault();
+      await signup().catch(console.error);
+      resetForm();
+    },
+    [resetForm, signup]
+  );
+  // const error =
+  //   data?.authenticateUserWithPassword.__typename ===
+  //   "UserAuthenticationWithPasswordFailure"
+  //     ? data?.authenticateUserWithPassword
+  //     : undefined;
+
+  return (
+    <Form method="POST" onSubmit={handleSubmit}>
+      <h2>Sign Up For An Account</h2>
+      <DisplayError error={error} />
+      <fieldset>
+        {data?.createUser && (
+          <p>
+            Signed up with {data.createUser.email} - Please Go ahead and Sign
+            in!
+          </p>
+        )}
+        <label htmlFor="name">
+          Your Name
+          <input
+            type="text"
+            name="name"
+            placeholder="Your name"
+            autoComplete="name"
+            value={inputs.name}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="email">
+          Email
+          <input
+            type="email"
+            name="email"
+            placeholder="Your email address"
+            autoComplete="email"
+            value={inputs.email}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="password">
+          Password
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            autoComplete="password"
+            value={inputs.password}
+            onChange={handleChange}
+          />
+        </label>
+        <button type="submit">Sign Up</button>
+      </fieldset>
+    </Form>
+  );
+};
+
+export default SignUpComponent;
--- frontend/pages/signin.js
+++ frontend/pages/signin.js
@@ -1,9 +1,18 @@
 import React from "react";
+import styled from "styled-components";
 import SignInComponent from "../components/SignInComponent";
+import SignUpComponent from "../components/SignUpComponent";
+
+const GridStyles = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  grid-gap: 2rem;
+`;
 
 const SignInPage = () => (
   <div>
     <SignInComponent />
+    <SignUpComponent />
   </div>
 );
 
