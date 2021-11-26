e20448104545eef9cb9a4ea10c936674deff5c17
Hayk Safaryan <hayk_safaryan@epam.com>
2021-11-26 13:54:48 +0100

    hotfix page count in pagination title

--- frontend/components/PaginationComponent.js
+++ frontend/components/PaginationComponent.js
@@ -28,7 +28,9 @@ const PaginationComponent = ({ page }) => {
   return (
     <PaginationStyles>
       <Head>
-        <title>Sick Fits - Page {page} of ___</title>
+        <title>
+          Sick Fits - Page {page} of {pageCount}
+        </title>
       </Head>
       <Link href={`/products/${page - 1}`}>
         <a aria-disabled={page <= 1}>&#129044; Prev</a>
